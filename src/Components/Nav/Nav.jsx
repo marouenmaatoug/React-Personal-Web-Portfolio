15fdf4f5245c524363bb20d76715b2be9c40229e
marouen maatoug <marouen.gimii@gmail.com>
2023-02-15 13:25:39 +0100

    Navigation Bar update

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,20 +1,52 @@
-import React from "react";
 import "./Nav.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
-import { useState } from "react";
+import { AiOutlineWallet } from "react-icons/ai";
+import { MdOutlineAutoAwesome } from "react-icons/md";
+import { useState, useEffect } from "react";
 
 const Nav = () => {
     const [activeNav, setActiveNav] = useState("#header");
+
+    const handleScroll = () => {
+        const position = window.scrollY + 100;
+        const navElements = document.querySelectorAll("nav a");
+
+        navElements.forEach((element) => {
+            const target = document.querySelector(element.hash);
+            const targetTop = target.getBoundingClientRect().top + window.scrollY;
+            const targetBottom = targetTop + target.offsetHeight;
+
+            if (position >= targetTop && position < targetBottom) {
+                setActiveNav(element.hash);
+            } else if (
+                window.innerHeight + window.scrollY >=
+                document.body.offsetHeight
+            ) {
+                setActiveNav("#contact");
+            }
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        // This return statement is used to remove event listener when the component is unmount
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    });
+
     return (
         <nav>
             <a
                 href="#header"
                 onClick={() => setActiveNav("#header")}
                 className={activeNav === "#header" ? "active" : ""}
+                title="Home"
             >
                 <AiOutlineHome />
             </a>
@@ -22,6 +54,7 @@ const Nav = () => {
                 href="#about"
                 onClick={() => setActiveNav("#about")}
                 className={activeNav === "#about" ? "active" : ""}
+                title="About"
             >
                 <AiOutlineUser />
             </a>
@@ -29,6 +62,7 @@ const Nav = () => {
                 href="#experience"
                 onClick={() => setActiveNav("#experience")}
                 className={activeNav === "#experience" ? "active" : ""}
+                title="Experience"
             >
                 <BiBook />
             </a>
@@ -36,13 +70,31 @@ const Nav = () => {
                 href="#services"
                 onClick={() => setActiveNav("#services")}
                 className={activeNav === "#services" ? "active" : ""}
+                title="Services"
             >
                 <RiServiceLine />
+            </a>
+            <a
+                href="#portfolio"
+                onClick={() => setActiveNav("#portfolio")}
+                className={activeNav === "#portfolio" ? "active" : ""}
+                title="Portfolio"
+            >
+                <AiOutlineWallet />
+            </a>
+            <a
+                href="#testimonials"
+                onClick={() => setActiveNav("#testimonials")}
+                className={activeNav === "#testimonials" ? "active" : ""}
+                title="Testimonials"
+            >
+                <MdOutlineAutoAwesome />
             </a>
             <a
                 href="#contact"
                 onClick={() => setActiveNav("#contact")}
                 className={activeNav === "#contact" ? "active" : ""}
+                title="Contact"
             >
                 <BiMessageSquareDetail />
             </a>
